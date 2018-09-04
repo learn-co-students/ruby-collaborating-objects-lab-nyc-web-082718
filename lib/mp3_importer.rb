559a5cdee1a047ea420698c99448f003e4c1fd9f
sinyoungnam <sinnam90@gmail.com>
2018-09-04 11:54:42 -0400

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,26 @@
+class MP3Importer
+
+  attr_accessor :path, :songs
+
+  def initialize(path)
+    @path = path
+    @songs = []
+
+    f = Dir["#{@path}/*.mp3"]
+    f.each do |song|
+      song_name = song.split("/")[-1]
+      @songs << Song.new(song_name).name
+    end
+  end
+
+  def files
+    @songs
+  end
+
+  def import
+    @songs.each do |song_file_name|
+      Song.new_by_filename(song_file_name)
+    end
+  end
+  
+end
